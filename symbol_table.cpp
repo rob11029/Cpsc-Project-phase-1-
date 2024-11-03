6e89e6c78e4892e6191a9cce3549c07286554905
Jaylynna Kann <jkann2004@csu.fullerton.edu>
2024-11-02 19:14:31 -0700

    Added symbol table for lexical and syntax analyzer

--- symbol_table.cpp
+++ symbol_table.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Node {
   string lexeme, token, value, char_start_num;
-  int lineNo, length;
+  int line_number, length;
   Node* next;
 
 public:
@@ -12,11 +12,11 @@ public:
   }
 
   // Node needs lexemes, token, lexeme value / lexeme itself, line number, character start number, and lexeme length
-  Node (string lexeme, string token, string value, int lineNo, char char_start_num, int length) {
+  Node (string lexeme, string token, string value, int line_number, char char_start_num, int length) {
     this->lexeme = lexeme;
     this->token = token;
     this->value = value;
-    this->lineNo = lineNo;
+    this->line_number = line_number;
     this->char_start_num = char_start_num;
     this->length = length;
     next = NULL;
@@ -27,7 +27,7 @@ public:
     cout << " Lexeme: " << lexeme
          << "\n Token: " << token
          << "\n Token Value: " << value
-         << "\n Line #: " << lineNo
+         << "\n Line #: " << line_number
          << "\n Character Start number: " << char_start_num
          << "\n Length: " << length << endl << endl;
   }
@@ -42,11 +42,11 @@ public:
     for (int i = 0; i < 100; i++) head[i] = NULL;
   }
   int hashf(string lexeme);
-  bool insert(string lexeme, string token, string value, int lineNo, char char_start_num, int length);
+  bool insert(string lexeme, string token, string value, int line_number, char char_start_num, int length);
   string find (string lexeme);
 };
 
-// Find the lexeme in the symbol table
+// Function to find the lexeme in the symbol table
 string SymbolTable::find(string l) {
   int index = hashf(l);
   Node* start = head[index];
@@ -62,6 +62,8 @@ string SymbolTable::find(string l) {
   return "-1";
 }
 
+// Function to insert lexemes, token, lexeme value / lexeme itself, line number, character start number,
+// and lexeme length into Symbol Table
 bool SymbolTable::insert(string l, string t, string v, int lNo, char c_s_n, int lgh) {
   int index = hashf(l);
   Node* p = new Node(l, t, v, lNo, c_s_n, lgh);
